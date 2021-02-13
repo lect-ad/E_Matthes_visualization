136d322651d279e5e3a6c7d088cbc203a2ed07ce
lect <litaniasr@gmail.com>
2021-02-13 15:43:55 +0300

    Started visualization project. Created random walk and its visualization.

---
+++ rw_visual.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+
+rw = RandomWalk()
+rw.fill_walk()
+
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=10)
+plt.show()
