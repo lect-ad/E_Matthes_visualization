37b9293a0ebe488c05dc65f99167faabf09b724e
lect <litaniasr@gmail.com>
2021-02-13 17:14:54 +0300

    Added visualization - histogram of single d6 rolls.

--- die_visual.py
+++ die_visual.py
@@ -1,9 +1,19 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 
 die =Die()
 num_rolls = 1000
 results = [die.roll() for _ in range(num_rolls)]
-frequencies = enumerate([results.count(value)
-                         for value in range(1, die.num_sides + 1)], start=1)
-print(*frequencies)
+frequencies = [results.count(value) for value in range(1, die.num_sides + 1)]
+
+x_values = list(range(1, die.num_sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title=f'Results of Rolling Single D6 Die {num_rolls} Times',
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='single_d6.html')
